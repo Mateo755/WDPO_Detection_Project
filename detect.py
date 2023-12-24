dc470895c846c57dc545b828f9a369bbdafd683d
Mateo755 <mateusz.polska07@gmail.com>
2023-12-24 14:22:32 +0100

    Detect file done

--- detect.py
+++ detect.py
@@ -6,6 +6,8 @@ import click
 import cv2
 from tqdm import tqdm
 
+from cvzone.ClassificationModule import Classifier
+
 
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
@@ -20,15 +22,54 @@ def detect(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         A dictionary with the number of each object.
     """
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
 
-    #TODO: Implement detection method.
-    
+    ## Model import
+    classifier = Classifier('Model/leaves_classifier_model.h5', 'Model/labels.txt')
+
+    ## Convert indexes of classes to names
+
+    TrainClasses = {'aspen': 0, 'birch': 1, 'hazel': 2, 'maple': 3, 'oak': 4}
+    ResultMap = {}
+    for idx, leaf in zip(TrainClasses.values(), TrainClasses.keys()):
+        ResultMap[idx] = leaf
+
     aspen = 0
     birch = 0
     hazel = 0
     maple = 0
     oak = 0
+
+    ## Image preprocessing
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(imgGray, 250, 255, cv2.THRESH_BINARY_INV)
+
+    imgBlur = cv2.GaussianBlur(thresh, (5, 5), 0)
+    imgCanny = cv2.Canny(imgBlur, 120, 120)
+
+    countours, hierarchy = cv2.findContours(imgCanny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in countours:
+        area = cv2.contourArea(cnt)
+        if area > 40:
+            x, y, w, h = cv2.boundingRect(cnt)
+            roi = img[y:y + h, x:x + w]
+
+            prediction, index = classifier.getPrediction(roi, draw=False)
+            leaf = ResultMap[index]
+
+            match leaf:
+                case "aspen":
+                    aspen += 1
+                case "birch":
+                    birch += 1
+                case "hazel":
+                    hazel += 1
+                case "maple":
+                    maple += 1
+                case "oak":
+                    oak += 1
 
     return {'aspen': aspen, 'birch': birch, 'hazel': hazel, 'maple': maple, 'oak': oak}
 
